2733ebae9e5fafa272bf26cb1d2348ad99b3854f
Inch129 <so0utt@gmail.com>
2015-08-05 09:05:47 -0700

    Добавлен скролл, при проигрывании музыки приложение закрывается с ошибкой. Нужно реализовать закрытие потока обновления бара в методе onDestroy().

--- app/src/main/java/com/example/mercury/player/Audio.java
+++ app/src/main/java/com/example/mercury/player/Audio.java
@@ -1,86 +1,125 @@
 package com.example.mercury.player;
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.util.Objects;
-
-import static com.example.mercury.player.R.raw.mysound;
 
 
-public class Audio extends Activity {
+public class Audio extends Activity implements SeekBar.OnSeekBarChangeListener {
 
     private MediaPlayer mediaPlayer;
-    private static final String TAG = "myLogs";
+    private static final String TAG = "pff";
+    private SeekBar bar;
+    private Thread updateSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "OnCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player);
         final Button btn = (Button) findViewById(R.id.bPlay);
         final TextView status = (TextView) findViewById(R.id.StatusView);
+        bar = (SeekBar) findViewById(R.id.seekBar);
+
+        //Проверка, что - бы песня не запускалась несколько раз при нажатии Play
+        //Создаем новый плеер, задаем параметры
+        if (mediaPlayer == null) {
+            Log.i(TAG, "Player == null");
+            Uri path = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mysound);
+            mediaPlayer = MediaPlayer.create(Audio.this, path);
+            //Создаем слушатель, который оповестит нас, когда песня закончится
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                //Обработка момента, когда песня заканчивается.
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    btn.setText("Play");
+                    status.setText("Status:Idle");
+                    bar.setProgress(0);
+                }
+            });
+        }
+
+        bar.setOnSeekBarChangeListener(this);
+
+        //обновляем слайдер в отдельном потоке
+        updateSeekBar = new Thread() {
+            @Override
+            public void run() {
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+                while (currentPosition < totalDuration) {
+                    try {
+                        sleep(500);
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        bar.setProgress(currentPosition);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //Когда песня заканчивается, закрываем поток.Позволяем обновить бар.
+                if (totalDuration == currentPosition) {
+                    Thread.interrupted();
+
+                }
+            }
+        };
+        updateSeekBar.start();
+        bar.setMax(mediaPlayer.getDuration());
 
 
+        //слушаем постукивания на кнопку, делаем выводы:
         btn.setOnClickListener(new View.OnClickListener() {
 
                                    @Override
                                    public void onClick(View v) {
-
-                                       //Проверка, что - бы песня не запускалась несколько раз при нажатии Play(не уверен в ее работоспособности, надо будет потестить)
-                                       //Создаем новый плеер, задаем параметры и
-                                       if (mediaPlayer == null) {
-                                           Uri path = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mysound);
-                                           mediaPlayer = MediaPlayer.create(Audio.this, path);
-
-                                           //Создаем слушатель, который оповестит нас, когда песня закончится
-                                           mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-
-                                               //Обработка момента, когда песня заканчивается.
-                                               @Override
-                                               public void onCompletion(MediaPlayer mp) {
-                                                   btn.setText("Play");
-                                                   status.setText("Status:Idle");
-
-                                               }
-                                           });
-                                       }
+                                       Log.i(TAG, "OnClick");
 
                                        //Ставим на паузу
                                        if (mediaPlayer.isPlaying()) {
                                            btn.setText("Play");
                                            status.setText("Status:Pause");
                                            mediaPlayer.pause();
+                                           Log.i(TAG, "Button:Pause");
+
                                        }
-                                       //Возобновление
+                                       //Возобновляем
                                        else {
+                                           Log.i(TAG, "Button:Play");
                                            btn.setText("Pause");
                                            status.setText("Status:Play");
                                            mediaPlayer.start();
-
                                        }
                                    }
-
-
                                }
 
         );
 
     }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.i(TAG, "onProgressChanged");
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        Log.i(TAG, "OnStartTrackingTouch");
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        Log.i(TAG, "OnStopTrackingTouch");
+        mediaPlayer.seekTo(seekBar.getProgress());
+    }
 
     @Override
     //Пользователь выходит из приложения, зачищаем и освобождаем всё, что только можем.
@@ -90,11 +129,12 @@ public class Audio extends Activity {
             mediaPlayer.stop();
             mediaPlayer.release();
             mediaPlayer = null;
-
+        //Придумай, как закрыть поток!
         }
 
 
         super.onDestroy();
     }
+
 
 }
